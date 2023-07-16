d7b3565361e5fd424be86f2ec2040372a499456f
Belish88 <belish888@rambler.ru>
2023-07-16 21:41:49 +0300

    added test_filter_executed and conftest

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,7 @@
 import pytest
 
 from setting.path import JSON
-from utils.functions import open_file
+from utils.functions import open_file, filter_executed
 
 
 def test_open_file():
@@ -9,5 +9,10 @@ def test_open_file():
     with pytest.raises(FileNotFoundError):
         open_file("gfghj")
 
+
+def test_filter_executed(fixture_filter_executed, fixture_one_executed):
+    assert len(filter_executed(fixture_filter_executed)) == 1
+    assert isinstance(filter_executed(fixture_filter_executed), list)
+    assert filter_executed(fixture_filter_executed) == fixture_one_executed
 
 
